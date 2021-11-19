2070a9e9c335f1ab62ae2e2296d8f77682d2aa73
Aubrey Jones <ajones@flagshipbio.com>
2021-11-18 18:45:37 -0600

    Reformated for comb binding, added location, npc, mook, and faction sheets.

---
+++ make_double.sh
@@ -0,0 +1,4 @@
+pdfunite npc_sheets.pdf npc_sheets.pdf npc_sheets_double.pdf
+pdfunite mooks_sheet.pdf mooks_sheet.pdf mooks_sheets_double.pdf
+pdfunite faction_sheets.pdf faction_sheets.pdf faction_sheets_double.pdf
+pdfunite locations_sheets.pdf locations_sheets.pdf locations_sheets_double.pdf
