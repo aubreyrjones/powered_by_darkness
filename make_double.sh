97df3f4403b64818541385f0dde86ea8d331ea49
Aubrey Jones <ajones@flagshipbio.com>
2021-11-20 12:23:59 -0600

    A bit more formatting, added gangrel bestial feature undo move, added page of clocks.

--- make_double.sh
+++ make_double.sh
@@ -2,3 +2,4 @@ pdfunite npc_sheets.pdf npc_sheets.pdf npc_sheets_double.pdf
 pdfunite mooks_sheet.pdf mooks_sheet.pdf mooks_sheets_double.pdf
 pdfunite faction_sheets.pdf faction_sheets.pdf faction_sheets_double.pdf
 pdfunite locations_sheets.pdf locations_sheets.pdf locations_sheets_double.pdf
+pdfunite clocks.pdf clocks.pdf clocks_double.pdf
